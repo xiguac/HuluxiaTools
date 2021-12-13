d27969571c41513012d264af64b4a6b57ab2333e
Xiguac <58496501+xiguac@users.noreply.github.com>
2021-12-13 10:50:55 +0800

    自动评论

--- SendMessage.php
+++ SendMessage.php
@@ -2,6 +2,6 @@
 include('login.php');
 
 //发布评论消息
-$msg = urlencode($msg); //url编码消息
+$msg = urlencode($msg); //url编码评论消息
 echo HttpPost("http://floor.huluxia.com/comment/create/ANDROID/2.0?platform=2&gkey=000000&app_version=4.1.0.1.1&versioncode=20141443&market_id=floor_tencent&_key=".$ukey."&device_code=000000000000000-0000-0000-0000-000000000000","post_id=".$id."&comment_id=0&text=".$msg."&patcha=&images=&remindUsers=");
 ?>
